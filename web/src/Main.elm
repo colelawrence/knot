8927761fb467dac0dc23165e4e5ded5751fa2e5e
Cole Lawrence <colelawr@gmail.com>
2019-03-13 19:11:27 -0400

    Create login session and show login url

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -1,8 +1,10 @@
-module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
-import Html exposing (Html, pre, text)
+import Html exposing (Html, pre, text, a)
+import Html.Attributes exposing (href)
 import Http
+import Json.Decode exposing (Decoder, field, string)
 
 
 authPublicUrl = "https://901.rkt.space"
@@ -23,21 +25,21 @@ main =
 
 -- MODEL
 
-
-type Model
+type StringResult
     = Failure
     | Loading
     | Success String
 
+type alias Model =
+    { loginAccess : StringResult
+    , loginUrl : StringResult
+    }
+
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Loading
-    , Http.post
-        { body = Http.emptyBody
-        , url = authPublicUrl ++ "/auth/v0/login/session"
-        , expect = Http.expectString GotText
-        }
+    ( Model Loading Loading
+    , getLoginAccessToken
     )
 
 
@@ -46,19 +48,27 @@ init _ =
 
 
 type Msg
-    = GotText (Result Http.Error String)
+    = GotLoginToken (Result Http.Error String)
+    | GotLoginUrl (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GotText result ->
+        GotLoginToken result ->
             case result of
-                Ok fullText ->
-                    ( Success fullText, Cmd.none )
+                Ok token ->
+                    ( { model | loginAccess = Success token }, getLoginUrl token )
 
                 Err _ ->
-                    ( Failure, Cmd.none )
+                    ( { model | loginAccess = Failure }, Cmd.none )
+        GotLoginUrl result ->
+            case result of
+                Ok url ->
+                    ( { model | loginUrl = Success url }, Cmd.none )
+
+                Err _ ->
+                    ( { model | loginUrl = Failure }, Cmd.none )
 
 
 
@@ -76,12 +86,45 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    case model of
+    case model.loginUrl of
         Failure ->
             text "I was unable to load your book."
 
         Loading ->
             text "Loading..."
 
-        Success fullText ->
-            pre [] [ text fullText ]
+        Success loginUrl ->
+            a [ href loginUrl ] [ text "Login with Google" ]
+
+
+
+-- HTTP
+
+getLoginAccessToken : Cmd Msg
+getLoginAccessToken =
+    Http.post
+        { body = Http.emptyBody
+        , url = authPublicUrl ++ "/auth/v0/login/session"
+        , expect = Http.expectJson GotLoginToken tokenDecoder
+        }
+
+getLoginUrl : String -> Cmd Msg
+getLoginUrl accessToken =
+    Http.request
+        { method = "POST"
+        , body = Http.emptyBody
+        , headers = [ Http.header "Authorization" ("Bearer " ++ accessToken) ]
+        , url = authPublicUrl ++ "/auth/v0/google/login_url"
+        , expect = Http.expectJson GotLoginUrl urlDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+tokenDecoder : Decoder String
+tokenDecoder =
+    field "access_token" string
+
+urlDecoder : Decoder String
+urlDecoder =
+    field "url" string
